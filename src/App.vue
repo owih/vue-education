489702aa65dd44333633def639fd690fb427d1d6
Golovatenko D.F <golovatenko@intervolga.ru>
2022-04-10 19:28:36 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style>
+</style>
